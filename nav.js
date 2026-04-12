f8c173bdf7f4bad617700ecfd8034b33c31b6ef3
Dillan <38782331+killstorm103@users.noreply.github.com>
2026-04-12 14:00:22 +0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,33 +1,17 @@
-// nav.js - Shared navigation bar (PS3‑compatible)
-
-(function() 
- {
-    var navHTML = '' +
-        '<div class="fixed-header">' +
-            '<h1>Rebugger - ToolBox: Updates & Patches</h1>' +
-            '<h1_under>Click on the images to download</h1_under>' +
-            '<div class="nav-container">' +
-                '<a href="https://github.com/killstorm103/Rebugger-ToolBox/blob/main/README.md" class="nav-button" target="_blank">How to install</a>' +
-                '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Changelog.html" class="nav-button" target="_blank">ChangeLogs</a>' +
-                '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Social_Medias.html" class="nav-button" target="_blank">Social Medias</a>' +
-                '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Services.html" class="nav-button" target="_blank">View Services</a>' +
-                '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Known.html" class="nav-button" target="_blank">Known cunts list</a>' +
-                '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Fixes_and_Other_Information.html" class="nav-button" target="_blank">Fixes & Other Information</a>' +
-            '</div>' +
+(function() {
+    // Just the fucking buttons. No <h1>, no bullshit subtitles.
+    var buttonsHTML = '' +
+        '<div class="nav-container">' +
+            '<a href="https://github.com/killstorm103/Rebugger-ToolBox/blob/main/README.md" class="nav-button" target="_blank">How to install</a>' +
+            '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Changelog.html" class="nav-button" target="_blank">ChangeLogs</a>' +
+            '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Social_Medias.html" class="nav-button" target="_blank">Social Medias</a>' +
+            '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Services.html" class="nav-button" target="_blank">View Services</a>' +
+            '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Known.html" class="nav-button" target="_blank">Known cunts list</a>' +
+            '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Fixes_and_Other_Information.html" class="nav-button" target="_blank">Fixes and Other Information</a>' +
         '</div>';
 
-    var placeholder = document.getElementById('shared-nav');
-
-    if (placeholder)
-    {
-        placeholder.innerHTML = navHTML;
-    }
-    else 
-    {
-        // Fallback: insert at the very top of the body
-        var body = document.body;
-        if (body) {
-            body.insertAdjacentHTML('afterbegin', navHTML);
-        }
+    var placeholder = document.getElementById('nav-buttons');
+    if (placeholder) {
+        placeholder.innerHTML = buttonsHTML;
     }
 })();
