1f9d7dcdd30b386128b7256e223befcb4ae98151
Dillan <38782331+killstorm103@users.noreply.github.com>
2026-04-12 14:12:28 +0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -3,11 +3,11 @@
     var buttonsHTML = '' +
         '<div class="nav-container">' +
             '<a href="https://github.com/killstorm103/Rebugger-ToolBox/blob/main/README.md" class="nav-button" target="_blank">How to install</a>' +
-            '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Changelog.html" class="nav-button" target="_blank">ChangeLogs</a>' +
-            '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Social_Medias.html" class="nav-button" target="_blank">Social Medias</a>' +
-            '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Services.html" class="nav-button" target="_blank">View Services</a>' +
-            '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Known.html" class="nav-button" target="_blank">Known cunts list</a>' +
-            '<a href="http://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Fixes_and_Other_Information.html" class="nav-button" target="_blank">Fixes and Other Information</a>' +
+            '<a href="https://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Changelog.html" class="nav-button" target="_blank">ChangeLogs</a>' +
+            '<a href="https://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Social_Medias.html" class="nav-button" target="_blank">Social Medias</a>' +
+            '<a href="https://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Services.html" class="nav-button" target="_blank">View Services</a>' +
+            '<a href="https://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Known.html" class="nav-button" target="_blank">Known cunts list</a>' +
+            '<a href="https://raw.githack.com/killstorm103/Rebugger-ToolBox/dev/Fixes_and_Other_Information.html" class="nav-button" target="_blank">Fixes and Other Information</a>' +
         '</div>';
 
     var placeholder = document.getElementById('nav-buttons');
