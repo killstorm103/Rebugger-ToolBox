2325f360a042e44604e5b7048985848a84de79f5
Dillan <38782331+killstorm103@users.noreply.github.com>
2026-04-13 20:57:53 +0800

    Update Platform_Checker.js

--- Scripts/Platform_Checker.js
+++ Scripts/Platform_Checker.js
@@ -97,5 +97,6 @@
     window.isPS4 = DetectPS4();
     window.ps3Firmware = window.isPS3 ? getPS3FirmwareVersion() : '';
     window.ps4Firmware = window.isPS4 ? getPS4FirmwareVersion() : '';
+    window.isPS3SilkBrowserEngine = window.isPS3 && window.ps3Firmware.indexOf('1') === 0;
     window.UserAgent = ua; 
 })();
